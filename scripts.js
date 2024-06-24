c5a7688f607ed4910901049ccef7cdb68dcd9c40
Catherine <xiayushan2003@gmail.com>
2024-06-23 23:30:10 -0700

    add index.html

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function compareProducts() {
+  const input = document.getElementById("searchInput").value;
+  if (input) {
+    alert("Comparing products: " + input);
+    // Implement the comparison logic here
+  } else {
+    alert("Please enter products to compare.");
+  }
+}
